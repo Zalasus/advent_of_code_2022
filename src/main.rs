e080ba0eeb86744cb5c7a8c6194388fc1f7dab10
Zalasus <niklas.weissner@gmail.com>
2022-12-02 21:38:06 +0100

    Copy main module from 2019

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,42 @@
 
-mod day1;
+pub mod day1;
+pub mod day2;
+
+use clap::Parser;
+
+use colored::Colorize;
+
+
+const DAYS: usize = 1;
+
+
+/// Zalasus' advent of code 2022 entry.
+#[derive(Parser)]
+struct Args {
+    #[clap(short, long)]
+    day: Option<usize>,
+
+    #[clap(short, long)]
+    interactive: bool,
+}
+
+fn run_day(day: usize, _args: &Args) {
+    println!("\n{} {}", "Running day".green().bold(), day);
+    match day {
+        1 => day1::run(),
+        _ => eprintln!("{} {}", "Unknown day: ".red().bold(), day),
+    }
+}
 
 fn main() {
-    day1::run();
+    let args = Args::parse();
+
+    if let Some(day) = args.day {
+        run_day(day, &args);
+    } else {
+        println!("{}", "[Running all days]".green().bold());
+        for day in 1..=DAYS {
+            run_day(day, &args);
+        }
+    }
 }
